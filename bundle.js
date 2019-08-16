8407087ac25febbdfa50b697ef00f145f0f0ed92
Junior Fernandes <juniorbass8@gmail.com>
2019-08-16 10:41:31 -0300

    Arrow function

--- bundle.js
+++ bundle.js
@@ -1,19 +1,14 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 9];
-var newArr = arr.map(function (item, index) {
-  return item + index;
+var arr = [1, 3, 4, 5, 8, 9]; //Aroow function
+
+var newArr = arr.map(function (item) {
+  return item * 2;
 });
-console.log(newArr);
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum);
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
+console.log(newArr); //calback - passagem de função pra outra função
+
+var test = function test() {
+  return 'teste';
+};
+
+console.log(test());
--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 const arr = [1, 3, 4, 5, 8, 9];
 
-const newArr = arr.map(function(item, index) {
-    return item + index;
-});
+//Aroow function
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-const sum = arr.reduce(function(total, next) {
-    return total + next;
-});
 
-console.log(sum);
+//calback - passagem de função pra outra função
 
-const filter = arr.filter(function(item) {
-    return item % 2 === 0;
-})
 
-console.log(filter);
 
-const find = arr.find(function(item) {
-    return item === 4;
-});
 
-console.log(find);
+const test = () => {
+    return 'teste';
+}
+
+console.log(test());
